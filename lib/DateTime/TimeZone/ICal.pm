58073ea2dc53a4afd9058cfda5163118b68cee3f
Dorian Taylor <dorian.taylor.lists@gmail.com>
2015-03-04 12:32:30 -0800

    woops readme from pod missing in dist

--- lib/DateTime/TimeZone/ICal.pm
+++ lib/DateTime/TimeZone/ICal.pm
@@ -29,11 +29,11 @@ DateTime::TimeZone::ICal - iCal VTIMEZONE entry to DateTime::TimeZone
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
